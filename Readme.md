cbec656d910c514de6ece54d59029690533f6b43
Mahmudul Hasan <mahmudbappy.pri@gmail.com>
2024-09-28 02:15:32 +0600

    Initial release

---
+++ Readme.md
@@ -0,0 +1,58 @@
+# gh-ibulk
+
+✨ An interactive GitHub (gh) CLI extension to delete multiple repos at once.
+
+<a href="https://github.com/mahmudz/gh-ibulk/releases"><img src="https://img.shields.io/github/release/mahmudz/gh-ibulk.svg" alt="Latest Release"></a>
+
+## 📦 Installing from Github CLI
+
+1. Install the `gh` CLI - see the [installation](https://github.com/cli/cli#installation)
+
+   _Installation requires a minimum version (2.0.0) of the GitHub CLI that supports extensions._
+
+2. Install this extension:
+
+   ```sh
+   gh extension install mahmudz/gh-ibulk
+   ```
+
+## ⚙️ Installing Manually
+
+> If you want to install this extension **manually**, follow these steps:
+
+1. Clone the repo
+
+   ```shell
+   # git
+   git clone https://github.com/mahmudz/gh-ibulk
+   ```
+
+   ```shell
+   # GitHub CLI
+   gh repo clone mahmudz/gh-ibulk
+   ```
+
+2. Cd into it
+
+   ```bash
+   cd gh-dash
+   ```
+
+3. Build it
+
+   ```bash
+   go build
+   ```
+
+4. Install it locally
+   ```bash
+   gh extension install .
+   ```
+
+## ⚡️ Usage
+
+Run & select your operation
+
+```sh
+gh ibulk
+```
--- main.go
+++ main.go
@@ -1,26 +1,292 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"log"
+	"math"
+	"net/url"
+	"os"
+	"strings"
 
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/cli/go-gh"
 	"github.com/cli/go-gh/v2/pkg/api"
 )
 
+var (
+	auth                struct{ Login string }
+	confirmedRandomText string
+)
+
 func main() {
-	fmt.Println("hi world, this is the gh-delete extension!")
+	checkTokenScope()
+
 	client, err := api.DefaultRESTClient()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error creating API client:", err)
+		os.Exit(0)
 		return
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
+
+	err = client.Get("user", &auth)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(0)
 		return
 	}
-	fmt.Printf("running as %s\n", response.Login)
+
+	for {
+		operation := selectOperation()
+
+		if operation == "exit" {
+			os.Exit(0)
+		}
+		repoOptions := fetchReposOptions(client)
+
+		if len(repoOptions) == 0 {
+			fmt.Println("No Repositories Found.")
+			continue
+		}
+
+		selectedRepositories := selectRepositories(repoOptions)
+
+		if len(selectedRepositories) == 0 {
+			fmt.Println("No Repositories Found.")
+			continue
+		}
+
+		fmt.Println("Selected Repositories: ")
+		for _, repo := range selectedRepositories {
+			fmt.Println(repo)
+		}
+
+		confirmText()
+
+		isConfirmed := confirmAction()
+
+		if isConfirmed {
+			err = spinner.New().
+				Title("Deleting repositories...").
+				Action(func() {
+					if operation == "delete" {
+						deleteRepos(client, selectedRepositories)
+					}
+				}).
+				Run()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func checkTokenScope() {
+	statusContent, _, err := gh.Exec("auth", "status")
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(0)
+	}
+
+	// Convert output to string and split into lines
+	outputStr := string(statusContent.String())
+	lines := strings.Split(outputStr, "\n")
+
+	// Find the line that contains "Token scopes"
+	var scopesLine string
+	for _, line := range lines {
+		if strings.Contains(line, "Token scopes") {
+			scopesLine = line
+			break
+		}
+	}
+
+	if scopesLine == "" {
+		fmt.Println("No Token scopes found in the output.")
+		os.Exit(0)
+	}
+
+	// Check if "delete_repo" is in the scopes
+	hasDeleteRepo := strings.Contains(scopesLine, "delete_repo")
+
+	if !hasDeleteRepo {
+		fmt.Println("You don't have 'delete_repo' scope in your gh token.")
+		fmt.Println("Refresh your token by running `gh auth refresh -s delete_repo` in your cli.")
+		os.Exit(0)
+	}
+}
+
+func randomBase64String(l int) string {
+	buff := make([]byte, int(math.Ceil(float64(l)/float64(1.33333333333))))
+	rand.Read(buff)
+	str := base64.RawURLEncoding.EncodeToString(buff)
+	return str[:l] // strip 1 extra character we get from odd length results
+}
+
+func selectOperation() string {
+	var value string
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("Choose operation").
+				Options(
+					huh.NewOption("Bulk: Delete repositories", "delete"),
+					huh.NewOption("Exit", "exit"),
+				).
+				Value(&value),
+		),
+	)
+
+	err := form.Run()
+
+	if err != nil {
+		os.Exit(0)
+	}
+
+	return value
+}
+
+func selectRepositories(repoOptions []huh.Option[string]) []string {
+	var selections []string
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewMultiSelect[string]().
+				Title("Select Repositories to Process").
+				Options(repoOptions...).
+				Filterable(false).
+				Value(&selections).
+				Height(8),
+		),
+	).
+		WithTheme(huh.ThemeDracula())
+
+	err := form.Run()
+
+	if err != nil {
+		os.Exit(0)
+	}
+
+	return selections
+}
+
+func confirmText() string {
+	randomText := randomBase64String(8)
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Confirm the text?").
+				Description(randomText).
+				Prompt("? ").
+				Validate(func(s string) error {
+					if s != randomText {
+						return errors.New("sorry, confirmation text didn't match")
+					}
+
+					return nil
+				}).
+				Value(&confirmedRandomText),
+		),
+	)
+
+	err := form.Run()
+
+	if err != nil {
+		os.Exit(0)
+	}
+
+	return confirmedRandomText
+}
+
+func confirmAction() bool {
+	var isConfirmed bool
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewConfirm().
+				Title("❌ Do you really want to delete the repositories? ❌").
+				Affirmative("Yes!").
+				Negative("No.").
+				Value(&isConfirmed),
+		),
+	)
+
+	err := form.Run()
+
+	if err != nil {
+		os.Exit(0)
+	}
+
+	return isConfirmed
+}
+
+func fetchReposOptions(client *api.RESTClient) []huh.Option[string] {
+	var searchQuery string
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Search").
+				Prompt("? ").
+				Description("Empty query will return 100 repositories").
+				Value(&searchQuery),
+		),
+	)
+
+	ierr := form.
+		Run()
+
+	if ierr != nil {
+		fmt.Println(ierr)
+		os.Exit(0)
+	}
+
+	fmt.Println("Fetching repositories...")
+
+	queryParams := url.QueryEscape(fmt.Sprintf("%s user:%s", searchQuery, auth.Login))
+	queryParams = strings.Trim(queryParams, " ")
+
+	var result map[string]interface{}
+	err := client.Get("search/repositories?q="+queryParams, &result)
+
+	if err != nil {
+		log.Panic("Error fetching repositories:", err)
+		os.Exit(0)
+	}
+
+	var opions []huh.Option[string]
+
+	// Extract repository names from the search result
+	if items, ok := result["items"].([]interface{}); ok {
+		for _, item := range items {
+			if repo, ok := item.(map[string]interface{}); ok {
+				if name, ok := repo["name"].(string); ok {
+					opions = append(opions, huh.NewOption(name, name))
+				}
+			}
+		}
+	}
+
+	return opions
+}
+
+func deleteRepos(client *api.RESTClient, repos []string) {
+	for _, repo := range repos {
+		var resp string
+		var err = client.Delete("repos/"+auth.Login+"/"+repo, &resp)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Deleted " + repo)
+	}
+}
